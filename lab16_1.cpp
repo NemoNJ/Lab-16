fd907425a9885f6c7e7cd750b36223a556da66de
NemoNJ <nemojirayu@gmail.com>
2024-02-02 17:03:04 +0700

    finish

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)/100.00;
+    }
+}
+void findRowSum(const double *a,double *b,int n,int m){
+     cout << fixed << showpoint << setprecision(2);
+     for(int i = 0;i < n;i++){
+        b[i] = 0;
+        for(int j = 0;j < m;j++){
+          b[i] += a[i*m+j];
+        }
+     }
+}
+void findColSum(const double *a,double *b,int n,int m){
+     cout << fixed << showpoint << setprecision(2);
+     for(int i = 0;i < m;i++){
+        b[i] = 0;
+        for(int j = 0;j < n;j++){
+          b[i] += a[j*m+i];
+        }
+     }
+}
